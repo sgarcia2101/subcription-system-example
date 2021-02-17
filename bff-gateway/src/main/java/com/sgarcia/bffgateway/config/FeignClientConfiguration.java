cee22b4edd2165bd0c0fbf49c70d387b29f36990
Unknown <sgarcia2101@alumno.uned.es>
2021-02-17 19:48:21 +0100

    Addign Custom Error Decoder in BFF

--- bff-gateway/src/main/java/com/sgarcia/bffgateway/config/FeignClientConfiguration.java
+++ bff-gateway/src/main/java/com/sgarcia/bffgateway/config/FeignClientConfiguration.java
@@ -1,13 +1,19 @@
 package com.sgarcia.bffgateway.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.web.server.ResponseStatusException;
 
 import feign.RequestInterceptor;
+import feign.Response;
+import feign.codec.ErrorDecoder;
 
 @Configuration
 public class FeignClientConfiguration {
@@ -22,4 +28,33 @@ public class FeignClientConfiguration {
 	public RequestInterceptor oauth2FeignRequestInterceptor() {
 		return new OAuth2FeignRequestInterceptor(new DefaultOAuth2ClientContext(), clientCredentialsResourceDetails());
 	}
+
+	@Bean
+	public CustomFeignErrorDecoder customDecoder() {
+		return new CustomFeignErrorDecoder();
+	}
+
+	public class CustomFeignErrorDecoder implements ErrorDecoder {
+
+		private final Logger log = LoggerFactory.getLogger(CustomFeignErrorDecoder.class);
+
+		private ErrorDecoder delegate = new ErrorDecoder.Default();
+
+		@Override
+		public Exception decode(String methodKey, Response response) {
+
+			log.trace("An exception has been caught in {}, trying to parse the playload.", methodKey);
+
+			if (response.body() == null) {
+				log.error("Failed to parse the playload: Response has no body.");
+				return delegate.decode(methodKey, response);
+			}
+
+			final HttpStatus status = HttpStatus.valueOf(response.status());
+
+			return new ResponseStatusException(status);
+
+		}
+	}
+
 }
